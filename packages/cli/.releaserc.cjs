5f85718a51a2c548b98d0c39a904fda0a4410fb0
Caio Pizzol <caiopizzol@icloud.com>
2025-11-24 10:42:55 -0300

    chore: fix prettier ignore

--- packages/cli/.releaserc.cjs
+++ packages/cli/.releaserc.cjs
@@ -8,7 +8,8 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/github',
